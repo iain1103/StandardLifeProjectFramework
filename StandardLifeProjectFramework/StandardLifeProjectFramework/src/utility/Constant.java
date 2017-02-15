28971cd15e4a15cd785b6f1bd822669226acd839
NCCGROUP\istorrie876 <iain.storrie@nccgroup.trust>
2017-02-15 14:09:43 +0000

    First Commit

---
+++ StandardLifeProjectFramework/StandardLifeProjectFramework/src/utility/Constant.java
@@ -0,0 +1,49 @@
+/* 
+* LIST OF METHODS CONTAINED IN THIS CLASS
+* 
+* URL
+* Path_TestData
+* File_TestData
+* Path_ScreenShot
+* Col_TestCaseName
+* Col_UserName
+* Col_Password
+* Col_Browser
+* 
+*/
+
+package utility;
+
+/* *******************************************************************
+* Author: Rory Cruickshank
+* Date: 30/03/2016
+* Function: Constant
+* Purpose: This method holds the constants which will be used for a test suite
+* Arguments: 
+* 			
+* Returns: 
+**********************************************************************
+* Change Log:
+* 
+* Date:
+* Author: 
+* Details:
+*
+*********************************************************************/
+public class Constant {
+
+    public static final String URL = "sl-intranet-int.company-net.com";
+    
+    public static final String Path_TestData = "C://Users//rcruickshank655//workspace//StandardLifeProjectFramework//src//testData//";
+    		 
+    public static final String File_TestData = "TestData.xlsx";
+    
+    public static final String Path_ScreenShot = "C://Users//rcruickshank655//workspace//StandardLifeProjectFramework//test-output//screenshots//";
+   
+	public static final int Col_TestCaseName = 0;	
+
+	public static final int Col_Browser = 1;
+	
+	public static final int Col_User = 2;
+    
+}
